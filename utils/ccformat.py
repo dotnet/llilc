e604c2b7959df75bd6f4d840693912eecc7ee05d
Pat Gavlin <pagavlin@microsoft.com>
2015-03-02 22:53:37 -0800

    Make ccFormat.py behave more nicely on non-Windows platforms.
    - Replace getopt usage with argparse and remove global variables
    - Add support for LibTooling compile command databases
    - Replace ad-hoc path combining with calls to os.path.join
    - Add command line arguments for the path to clang-tidy,
      clang-format-diff, and the various build and source directories.

--- utils/ccformat.py
+++ utils/ccformat.py
@@ -1,112 +1,23 @@
+#!/usr/bin/env python
+
 import sys
-import getopt
+import argparse
 import os
 import subprocess
-import shutil
+import platform
 
-llilcSource = ""
-llvmSource = ""
-llvmBuild = ""
-llilcLib = ""
-llilcInc = ""
-clrInc = ""
-llvmInc = ""
-formatFix = ""
-tidyFix = ""
-doFormat = True
-tidy = True
-base = "origin"
-tidyChecks = "llvm*,misc*,microsoft*"
+def runTidy(args):
+  if args.llilc_source == None:
+    print >> sys.stderr, "Please specify --llilc-source or set the " \
+                          "LLILC_SOURCE environment variable."
+    return 1
 
-def checkEnvironment():
-  if os.environ.get('LLVMSOURCE') == None:
-    print "No LLVMSOURCE"
-    return 2
-  else:
-    global llvmSource
-    llvmSource = os.environ['LLVMSOURCE']
-
-  if os.environ.get('LLVMBUILD') == None:
-    print "No LLVMBUILD"
-    return 2
-  else:
-    global llvmBuild
-    llvmBuild = os.environ['LLVMBUILD']
-
-  global llilcSource
-  if os.environ.get('LLILCSOURCE') == None:
-    llilcSource = os.environ['LLVMSOURCE'] + "\\tools\llilc"
-  else:
-    llilcSource = os.environ['LLILCSOURCE']
-
-  global llilcLib 
-  llilcLib = llilcSource + "\lib"
-  global llilcInc
-  llilcInc = llilcSource + "\include"
-  global clrInc
-  clrInc = llilcInc + "\clr"
-  global llvmInc
-  llvmInc = llvmSource + "\include"
-
-  return 0
-
-def parseArgs(argv):
-  try:
-    opts, args = getopt.getopt(argv, "h", ["fix", "checks=", "base=", "help",
-        "untidy", "noformat"])
-  except getopt.GetoptError as err:
-    print err
-    return 3
-
-  for opt, arg in opts:
-    if opt in ("--fix"):
-      global formatFix
-      formatFix = "-i"
-      global tidyFix
-      tidyFix = "-fix"
-    elif opt in ("--untidy"):
-      global tidy
-      tidy = False
-    elif opt in ("--noformat"):
-      global doFormat
-      doFormat = False
-    elif opt in ("--checks"):
-      global tidyChecks
-      tidyChecks = arg
-    elif opt in ("--base"):
-      global base
-      base = arg
-    elif opt in ("-h", "--help"):
-      return usage()
-  
-  return 0
-
-def usage():
-  print "Usage: python ccFormat.py [--checks] [--fix] [-h]"
-  print "\t--fix\t\tFix failures when possible"
-  print "\t--untidy\t\tDon't run clang-tidy"
-  print "\t--noformat\t\tDon't run clang-format"
-  print "\t--checks=<chk>\tClang Tidy checks to run"
-  print "\t--base=<base>\tBase for obtaining diffs"
-  print "\t--help\t\tDisplay this help message."
-
-  print ""
-  print "Requirements:"
-  print "\tEnvirontment variables: LLVMSOURCE, LLVMBUILD"
-  print "\tTools (on path): clang-format.exe, clang-tidy.exe " \
-      + "clang-format-diff.py"
-  print "\tTool dependencies: Python 2.7 or later"
-  print ""
-  return 4
-
-def runTidy():
-  if not tidy:
-    return 0
+  tidyFix = "-fix" if args.fix else ""
 
   inc = ""
   tempinc = ""
 
-  if  os.environ.get('INCLUDE') != None:
+  if os.environ.get('INCLUDE') != None:
     tempinc = os.environ['INCLUDE']
 
   if tempinc != "":
@@ -114,67 +25,113 @@ def runTidy():
     for item in tempincList:
       inc = inc + "-issytem " + item
 
-  flags = "-target x86_64-pc-win32-msvc -fms-extensions -fms-compatibility \
-      -fmsc-version=1800"
-  inc = inc + " -I" + llilcLib + "\Jit -I" + llilcLib + "\MSILReader -I" \
-      + llilcInc + " -I" + llvmInc + " -I" + clrInc + " -I" + llilcInc \
-      + "\Driver -I" + llilcInc + "\Jit -I" + llilcInc + "\Reader -I" \
-      + llilcInc + "\Pal -I" + llvmBuild + "\\tools\LLILC\lib\Reader -I" \
-      + llvmBuild + "\\tools\LLILC\include -I" + llvmBuild + "\include"
-  setDef = "-D_DEBUG -D_CRT_SECURE_NO_DEPRECATE -D_CRT_SECURE_NO_WARNINGS " \
-      + "-D_CRT_NONSTDC_NO_DEPRECATE -D_CRT_NONSTDC_NO_WARNINGS " \
-      + "-D_SCL_SECURE_NO_DEPRECATE -D_SCL_SECURE_NO_WARNINGS " \
-      + "-D__STDC_CONSTANT_MACROS -D__STDC_FORMAT_MACROS " \
-      + "-D__STDC_LIMIT_MACROS -D_GNU_SOURCE -DLLILCJit_EXPORTS -D_WINDLL " \
-      + "-D_MBCS -DNOMINMAX"
+  clang_args = ""
+  if args.compile_commands == None:
+    # TODO: platform checks and defines
 
-  currentDirectory = os.getcwd()
-  os.chdir(llilcSource)
+    if args.llvm_source == None:
+      print >> sys.stderr, "Please specify --llvm-source or set the " \
+                           "LLVM_SOURCE environment variable."
+      return 1
 
-  basedir = os.getcwd()
+    if args.llvm_build == None:
+      print >> sys.stderr, "Please specify --llvm-build or set the " \
+                           "LLVM_BUILD environment variable."
+      return 1
+
+    llilcLib = os.path.join(args.llilc_source, "lib")
+    llilcInc = os.path.join(args.llilc_source, "include")
+    clrInc = os.path.join(llilcInc, "clr")
+    llvmInc = os.path.join(args.llvm_source, "include")
+
+    flags = "-target x86_64-pc-win32-msvc -fms-extensions -fms-compatibility \
+        -fmsc-version=1800"
+    inc = inc + " -I" + os.path.join(llilcLib, "Jit") \
+        + " -I" + os.path.join(llilcLib, "MSILReader") \
+        + " -I" + llilcInc + " -I" + llvmInc + " -I" + clrInc \
+        + " -I" + os.path.join(llilcInc, "Driver") \
+        + " -I" + os.path.join(llilcInc, "Jit") \
+        + " -I" + os.path.join(llilcInc, "Reader") \
+        + " -I" + os.path.join(llilcInc, "Pal") \
+        + " -I" + os.path.join(args.llvm_build, "tools", "LLILC", "lib", "Reader") \
+        + " -I" + os.path.join(args.llvm_build, "tools", "LLILC", "include") \
+        + " -I" + os.path.join(args.llvm_build, "include")
+    setDef = "-D_DEBUG -D_CRT_SECURE_NO_DEPRECATE -D_CRT_SECURE_NO_WARNINGS " \
+        + "-D_CRT_NONSTDC_NO_DEPRECATE -D_CRT_NONSTDC_NO_WARNINGS " \
+        + "-D_SCL_SECURE_NO_DEPRECATE -D_SCL_SECURE_NO_WARNINGS " \
+        + "-D__STDC_CONSTANT_MACROS -D__STDC_FORMAT_MACROS " \
+        + "-D__STDC_LIMIT_MACROS -D_GNU_SOURCE -DLLILCJit_EXPORTS -D_WINDLL " \
+        + "-D_MBCS -DNOMINMAX"
+    clang_args = "-- " + flags + " " + inc + " " + setDef
+  else:
+    clang_args = "-p " + args.compile_commands
+
   returncode = 0
-
-  for dirname,subdir,files in os.walk(basedir):
+  for dirname,subdir,files in os.walk(args.llilc_source):
     for filename in files:
       if filename.endswith(".c") or filename.endswith(".cpp"):
-        errorlevel = subprocess.call("clang-tidy " + tidyFix + " -checks="
-            + tidyChecks + " -header-filter=\"LLILC.*(Reader)|(Jit)|(Pal)\" "
-            + dirname + "\\" + filename + " -- " + flags + " " + inc + " " 
-            + setDef, shell=True)
+        filepath = os.path.join(dirname, filename)
+        errorlevel = subprocess.call(args.clang_tidy_binary + " " + tidyFix
+            + " -checks=" + args.checks
+            + " -header-filter=\"" + args.llilc_source
+            + ".*(Reader)|(Jit)|(Pal)\" " + filepath
+            + " " + clang_args, shell=True)
         if errorlevel == 1:
           returncode = 1
   
-  os.chdir(currentDirectory)
   return returncode
 
-def runFormat():
-  if not doFormat:
-    return 0
-
-  subprocess.call("git diff " + base + "-U0 2>nul | clang-format-diff.py -p1 "
-      + formatFix, shell=True)
+def runFormat(args):
+  formatFix = "-i" if args.fix else ""
+  subprocess.call("git diff " + args.base + "-U0 2>nul | "
+      + args.clang_format_diff + " -p1 " + formatFix, shell=True)
   return 0
 
 def main(argv):
-    returncode = parseArgs(argv)
-    if returncode != 0:
-        return returncode
-    
-    returncode = checkEnvironment()
-    if returncode != 0:
-        return returncode
+  parser = argparse.ArgumentParser(description=
+                   "Report and optionally fix formatting errors in the LLILC "
+                   "sources.")
+  parser.add_argument("--clang-tidy-binary", metavar="PATH",
+            default="clang-tidy", help="path to clang-tidy binary")
+  parser.add_argument("--clang-format-diff", metavar="PATH",
+            default="clang-format-diff.py",
+            help="path to clang-format-diff tool")
+  parser.add_argument("--compile-commands", metavar="PATH",
+            help="path to a directory containing a JSON compile commands " \
+                 "database used to determine clang-tidy options")
+  parser.add_argument("--llvm-source", metavar="PATH",
+            default=os.environ.get("LLVMSOURCE"),
+            help="path to LLVM sources (only necessary when a compile " \
+                 "commands database has not been specified)")
+  parser.add_argument("--llvm-build", metavar="PATH",
+            default=os.environ.get("LLVMBUILD"),
+            help="path to LLVM build (only necessary when a compile commands " \
+                 "database has not beenspecified)")
+  parser.add_argument("--llilc-source", metavar="PATH",
+            default=os.path.join(os.path.dirname(__file__), ".."),
+            help="path to LLILC sources")
+  parser.add_argument("--fix", action="store_true", default=False,
+            help="fix failures when possible")
+  parser.add_argument("--untidy", action="store_true", default=False,
+            help="Don\'t run clang-tidy")
+  parser.add_argument("--noformat", action="store_true", default=False,
+            help="Don\'t run clang-format-diff")
+  parser.add_argument("--checks", default="llvm*,misc*,microsoft*",
+            help="clang-tidy checks to run")
+  parser.add_argument("--base", metavar="BRANCH", default="origin",
+            help="Base for obtaining diffs")
+  args = parser.parse_args(argv)
 
-    returncode = runTidy()
-    runFormat()
-    
-    return returncode
+  returncode=0
+  if not args.untidy:
+    returncode = runTidy(args)
+    if returncode != 0:
+      return returncode
+
+  if not args.noformat:
+    runFormat(args)
+  
+  return returncode
 
 if __name__ == "__main__":
-  returncode = main(sys.argv[1:])
-
-  if returncode == 4:
-    # if we return 4, we printed the help message and should just quit,
-    # but not fail
-    returncode = 0
-
-  sys.exit(returncode)
+  main(sys.argv[1:])
