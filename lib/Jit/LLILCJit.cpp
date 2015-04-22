d55421b8066d7fe9fdc668a24b7e1b5162e8e215
Joseph Tremoulet <jotrem@microsoft.com>
2015-04-22 17:02:04 -0400

    Discard functions with landing pads
    This is a temporary measure during EH bring-up; methods with landing pads
    will have their IR shown (if IR dumps are enabled), but then will be
    discarded so we'll fall back to the other jit rather than choking on the
    landing pads downstream.

--- lib/Jit/LLILCJit.cpp
+++ lib/Jit/LLILCJit.cpp
@@ -361,6 +361,17 @@ bool LLILCJit::readMethod(LLILCJitContext *JitContext) {
 
   JitContext->MethodName = FuncName;
 
+  if (IsOk) {
+    for (BasicBlock &BB : *Func) {
+      if (BB.getLandingPadInst() != nullptr) {
+        // Don't try to push the EH code downstream (we'll currently fail on
+        // the first method with EH if we do that, which prevents being able
+        // to see IR for later methods)
+        return false;
+      }
+    }
+  }
+
   return IsOk;
 }
 
