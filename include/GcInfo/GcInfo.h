8dcbe046cee22d9285e4f9116dfc05ba379f093d
Swaroop Sridhar <Swaroop.Sridhar@microsoft.com>
2015-04-29 20:29:54 -0700

    Use GcInfo Encoding in LLILC
    This change build LLILC with CoreCLR's GcInfoEncoding Library.
    -> The GcInfoEncoder headers and implementation are available in CoreCLR's
       Output directory.
    -> include\GcInfoUtil provides (alternate implementations for) some
       data-structures and utilities used by the encoder
    -> LLILC generates GcInfo correctly using the encoder.
    
    GcInfoUtil provides the following utilities used by the GcInfoEncoder:
    1) Logging functions used by the encoder
    2) Helpers for bit-manipulation and overflow checking
    3) A simple allocator (using new and delete)
    4) SList -- a singly linked list that supports insert-at-tail.
       This is a subset of SList in CoreCLR tree, with only necessary functionality.
    5) CQuickSort - A simple quick sort implementation with comparer
       overridden by the GcInfo encoder library -- from the CoreClr tree.
    6) StructArrayList -- a list of arrays which has specific growth and location
       preserving characteristics -- simplified version from CoreCLR tree.
    7) SimplerHashTable -- a wrapper around standard unordered map, with
       the encoder's own hashing and comparison functions.
    
    Testing:
    
    All LLILC Unit tests passed locally
    Rosly builds without the need for code-size workarounds.

---
+++ include/GcInfo/GcInfo.h
@@ -0,0 +1,22 @@
+//===---- include/gcinfo/gcinfo.h -------------------------------*- C++ -*-===//
+//
+// LLILC
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// \brief Wrapper that includes all GcInfo related headers
+///
+//===----------------------------------------------------------------------===//
+
+#ifndef GCINFO_H
+#define GCINFO_H
+
+#define STANDALONE_BUILD
+#include "gcinfoencoder.h"
+
+#endif // GCINFO_H
