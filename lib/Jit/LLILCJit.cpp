8be9054ec91db9d4cd67efc4de27c8bda5f1b5a5
Joseph Tremoulet <jotrem@microsoft.com>
2015-07-08 14:47:52 -0400

    Verify and dump entire module, not just function.
    This will be necessary when we outline filter functions.

--- lib/Jit/LLILCJit.cpp
+++ lib/Jit/LLILCJit.cpp
@@ -399,8 +399,7 @@ bool LLILCJit::readMethod(LLILCJitContext *JitContext) {
     return false;
   }
 
-  Function *Func = JitContext->CurrentModule->getFunction(FuncName);
-  bool IsOk = !verifyFunction(*Func, &dbgs());
+  bool IsOk = !verifyModule(*JitContext->CurrentModule, &dbgs());
   assert(IsOk && "verification failed");
 
   if (IsOk) {
@@ -415,7 +414,7 @@ bool LLILCJit::readMethod(LLILCJitContext *JitContext) {
   }
 
   if (DumpLevel == ::DumpLevel::VERBOSE) {
-    Func->dump();
+    JitContext->CurrentModule->dump();
   }
 
   return IsOk;
