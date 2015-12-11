8b21462dcaf143104ca26ab836780db33666e943
Swaroop Sridhar <Swaroop.Sridhar@microsoft.com>
2015-12-10 16:13:45 -0800

    Add CoreDisTools Library
    This change adds CoreDisTools -- a library containing
    LLVM disassembly tools for CoreClr to LLILC repo.
    
    Currently, the library supports instruction-wise
    disassembly API required for implementing GcStress.
    It also provides API for PrettyPrinting the
    disassembled instructions.
    
    coredistools.h -- the export/import header
    coredistools.cpp -- the implementation as a wrapper around
        LLVM disassemblers to suite the required API.
    
    This change also adds code to package the library into a
    NuGet package.

---
+++ include/CoreDisTools/coredistools.h
@@ -0,0 +1,64 @@
+//===--------- coredistools.h - Dissassembly tools for CoreClr
+//-------------===//
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// \brief Core Disassembly Tools API for CoreClr AOT/JIT
+///
+//===----------------------------------------------------------------------===//
+
+#if !defined(LLILC_TOOLS_COREDISTOOLS)
+#define LLILC_TOOLS_COREDISTOOLS
+
+#include <stdint.h>
+
+#if defined(__cplusplus)
+#define EXTERN_C extern "C"
+#else
+#define EXTERN_C
+#endif // defined(__cplusplus)
+
+#if defined(_MSC_VER)
+#if defined(DllInterfaceExporter)
+#define DllIface EXTERN_C __declspec(dllexport)
+#else
+#define DllIface EXTERN_C __declspec(dllimport)
+#endif // defined(DllInterfaceExporter)
+#else
+#define DllIface
+#endif // defined(_MSC_VER)
+
+enum TargetArch {
+  Target_Host, // Target is the same as host architecture
+  Target_X86,
+  Target_X64,
+  Target_Thumb,
+  Target_Arm64
+};
+
+struct CorDisasm;
+
+DllIface CorDisasm *InitDisasm(enum TargetArch Target);
+
+// DisasmInstruction -- Disassemble one instruction
+// Arguments:
+// Address -- The address at which the bytes of the instruction
+//            are intended to execute
+// Bytes -- Pointer to the actual bytes which need to be disassembled
+// MaxLength -- Number of bytes available in Bytes buffer
+// PrintAssembly -- Whether to dump the decoded instriction to stdout
+// Returns:
+//   -- The Size of the disassembled instruction
+//   -- Zero on failure
+DllIface size_t DisasmInstruction(const CorDisasm *Disasm, size_t Address,
+                                  const uint8_t *Bytes, size_t Maxlength,
+                                  bool PrintAssembly);
+
+DllIface void FinishDisasm(const CorDisasm *Disasm);
+
+#endif // !defined(LLILC_TOOLS_COREDISTOOLS)
